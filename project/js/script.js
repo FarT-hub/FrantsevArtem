7548db32ed36d303457e32c6ba8b1759efcca8cf
Frantsev Artem <fran_lg@ukr.net>
2020-12-15 01:58:29 +0200

    Homework 24/25

---
+++ project/js/script.js
@@ -0,0 +1,15 @@
+
+$('.home-slider').owlCarousel({
+    animateOut: 'fadeOut',
+    items:1,
+    loop:true,
+    dots:false,
+    autoplayHoverPause: false,
+    autoplay: true,
+    smartSpeed: 1000,
+    nav:true,
+    navText:[
+        '<i class="fa fa-angle-left"></i>',
+        '<i class="fa fa-angle-right"></i>'
+    ],    
+})
